61568383dab02c306e5e84e1b218ec95ed33fee0
Naveen7505 <naveenlodhi1977@gmail.com>
2025-04-05 12:00:16 +0530

    Update moviesData.js

--- moviesData.js
+++ moviesData.js
@@ -1,30 +1,30 @@
 export const movies = [{
-    title: "The Avengers",
-    plot: "Plot of the movie is related to super natural powers",
+    title: "Stree 2",
+    plot: "Sarkate Ka Aatank is a horror-comedy where Chanderi faces a new supernatural threat, forcing Vicky and his friends to unravel its chilling mystery.",
     price: 199,
     rating: 8.0,
-    poster: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    poster: "https://m.media-amazon.com/images/M/MV5BMTA1NmUxYzItZmVmNy00YmQxLTk4Y2UtZjVkMWUwMWQ5N2IxXkEyXkFqcGc@._V1_.jpg",
     stars: 0,
     fav:false,
     isInCart:false
     },
 
     {
-    title: "Ironman",
-    plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    title: "Dunki",
+    plot: "Dunki is a heartfelt adventure drama about four friends from Punjab who dream of reaching England but, lacking visas, embark on a risky illegal immigration journey known as donkey flight.",
     price: 399,
     rating: 8.9,
-    poster: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    poster: "https://m.media-amazon.com/images/I/71IHuX3PD0L.jpg",
     stars: 0,
     fav:false,
     isInCart:false
     },
     {
-    title: "The Avengers",
-    plot: "Plot of the movie is related to super natural powers",
+    title: "Bade Miyan Chote Miyan",
+    plot: "Bade Miyan Chote Miyan is a high-octane action thriller where two elite soldiers must stop a masked villain from unleashing chaos on India's defenses.",
     price: 199,
     rating: 8.0,
-    poster: "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    poster: "https://akm-img-a-in.tosshub.com/indiatoday/styles/medium_crop_simple/public/2024-01/419931115_295123099824358_5999368769804219535_n.jpg?VersionId=F4fewocAuG6cbDQFEaxobtsnqcdX_b8m&size=750:*",
     stars: 0,
     fav:false,
     isInCart:false
